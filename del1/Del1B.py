5988fe5fbfb856eb6116704310d2e7c0f196c676
hishemok <102466484+hishemok@users.noreply.github.com>
2022-12-02 15:16:31 +0100

    Update Del1B.py

--- del1/Del1B.py
+++ del1/Del1B.py
@@ -1,7 +1,9 @@
+#EGEN KODE
 import numpy as np
 from tqdm import trange
 from numba import njit
 
+#INITIAL VALUES
 N = 10**5
 L = 10**(-6)
 T = 3000
@@ -10,6 +12,7 @@ dt = 10**(-12)
 k = 1.38*10**(-23)
 m = 3.3*10**(-27)
 
+#RANDOM POSITION AND VELOCITY WITHIN BOX
 def init_pos_vel(N,L,T):
     r = np.random.rand(N,3)*L
     v = np.random.normal(0, np.sqrt(k*T/m), size=(N, 3))
@@ -17,12 +20,13 @@ def init_pos_vel(N,L,T):
 
 r,v = init_pos_vel(N,L,T)
 
-
+#INTEGRATION LOOP
 @njit
 def loop(N,L,r,v,dt): 
     r = r + v*dt
     for j in range(N):
-
+        #CHECK IF PARTICLE MOVED OUT OF BOX
+        #IF SO SWITCH DIRECTION OF PARTICLE TO KEEP IN
         for k in range(3):
             if r[j,k] <= 0:
                 v[j,k] = -v[j,k]
