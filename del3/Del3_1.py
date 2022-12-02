c180f3ee706669de26a7738d3ba73cf470cdf5af
hishemok <102466484+hishemok@users.noreply.github.com>
2022-12-02 16:00:42 +0100

    Update Del3_1.py

--- del3/Del3_1.py
+++ del3/Del3_1.py
@@ -14,16 +14,15 @@ T = system.star_temperature
 
 r = utils.AU_to_m(np.linalg.norm(np.einsum('ij->ji',system.initial_positions)[0]))
 rs = system.star_radius*10**3 #m
-A = 4*np.pi*rs**2
-dA = A/(4*np.pi*r**2)
+A = 4*np.pi*rs**2 #AREA
+dA = A/(4*np.pi*r**2) #PART OF AREA
 
 
 F = (rs/r)**2*sigma*T**4
 W = 40/0.12
-Ap = 2*np.pi*(system.radii[0]*10**3)**2
-#print(F*Ap)
-print(W/F)
+Ap = 2*np.pi*(system.radii[0]*10**3)**2 #AREAPLANET AS IF ITS A CIRCLE
 
+#CALC FLUX
 def flux_on_planet(r,R):
     F = (rs/r)**2*sigma*T**4
     A = 2*np.pi*R**2
@@ -32,19 +31,19 @@ def flux_on_planet(r,R):
 #test planet 0
 #print(flux_on_planet(r,system.radii[0]*10**3))
 #same as above
+#WEE
 
 def planet_temperature(flux):
     return (flux/(sigma*2*np.pi*R**2))**(1/4)
 
-
-habitable = []
+habitable = []#LOOK FOR HABITABLE PLANETS
 for planet_idx in range(system.number_of_planets):
     r = utils.AU_to_m(np.linalg.norm(np.einsum('ij->ji',system.initial_positions)[planet_idx]))
     R = system.radii[0]*10**3
     flux = flux_on_planet(r,R)
     print(f'Planet {planet_idx}: T {planet_temperature(flux):.3f} K')
     if planet_temperature(flux) > 245 and planet_temperature(flux) < 410:
-        # print(f'Planet {planet_idx} is habitable ')
+        print(f'Planet {planet_idx} is habitable ')
         habitable.append(planet_idx)
 print('')
 for i in habitable:
